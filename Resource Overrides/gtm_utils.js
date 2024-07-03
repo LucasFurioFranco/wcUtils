b3656dff234d55975ef43323f49ced97d81d3157
Lucas Fúrio Franco <lucasfuriofranco@gmail.com>
2024-07-03 10:50:44 -0300

    Create gtm_utils.js

---
+++ Resource Overrides/gtm_utils.js
@@ -0,0 +1,34 @@
+(function() {
+    var allow_log = true;
+
+    function logger() {
+        if (allow_log) console.log.apply(null, arguments);
+    }
+
+    if (location.hostname.indexOf("tagmanager.google.com") >= 0) {
+
+        window.addEventListener("keydown", function(evt) {
+
+            logger("### I Hear You");
+
+            if (evt.ctrlKey && evt.key == "Enter") {
+                logger("### ### CTRL+Enter");
+
+                //document.body.focus(); //Don't ask :'(
+                var sv_btn = Array.from(document.querySelectorAll('[data-gtm-submit-button]')).pop();
+                sv_btn && sv_btn.click();
+
+            } else if (evt.key == 'F2') {
+                logger("### ### F2");
+
+                var name_in = Array.from(document.querySelectorAll('[data-name-value]')).pop(); //Retrieves the çast "save" button
+                name_in && name_in.focus();
+
+            }
+
+        }, true);
+
+
+
+    }
+})()
